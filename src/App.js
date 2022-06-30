e0d9eea96003c437f10b26eedf6b29666f338685
Ismael Marquez <imarquez99@gmail.com>
2022-06-30 19:18:27 -0300

    listing items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      items: [],
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:8080/api/v1/items')
+    .then((response) => response.json())
+    .then((items) => this.setState(() => {
+      return {items: items}
+    },
+    () => {
+        console.log(this.state);
+      }
+    ));
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.items.map((item) => {
+          return (
+            <div key={item.codigo}>
+              <h1>{item.codigo} - {item.nombre}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
+   
 
 export default App;
